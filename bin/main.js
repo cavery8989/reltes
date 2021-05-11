b113ae70555ab254972d70da097abd55425f3724
cavery8989 <c.avery1989@gmail.com>
2021-05-11 23:07:58 +0100

    ts

---
+++ bin/main.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+"use strict";
+exports.__esModule = true;
+exports.func = void 0;
+console.log('hello world');
+var func = function (x) {
+    return x + 10;
+};
+exports.func = func;
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 console.log('hello world')
 
 export const func = (x: number) => {
